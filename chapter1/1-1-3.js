deb229a3582468cad33417c24ef8ac094c0354b2
akersof <akersof@gmail.com>
2019-04-17 14:10:32 +0200

    ex 1-1-3 done.

--- chapter1/1-1-3.js
+++ chapter1/1-1-3.js
@@ -1,5 +1,5 @@
 /*
-    Check is a word passed as a parameter is a palindrome
+    Check id a word passed as a parameter is a palindrome
  */
 
 const isPalindrome = str => {
@@ -7,12 +7,14 @@ const isPalindrome = str => {
     if(str.length === 0 || str.length === 1) return true;
     //Failure terminal case
     else if(str[0] !== str[str.length -1]) return false;
+    //str can still be a palindrome so far, let recurse a step more
     else
-        //Step
         return isPalindrome(str.substring(1,str.length - 1));
 };
 
 
+// The string to test is passed as a command line parameter
+// e.g. node 1-1-3.js RADAR
 let str = process.argv[2].split(" ").join("");
 
 if (isPalindrome(str))
