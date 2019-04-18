452fb608af5d57e7cb8dd3acfb7ded6c6166db6e
akersof <akersof@gmail.com>
2019-04-18 02:55:44 +0200

    continuing 1.2.2 exercice

--- chapter1/1-2-2.js
+++ chapter1/1-2-2.js
@@ -13,12 +13,12 @@ Définir un algorithme pour résoudre ce problème quel que soit le nombre de tr
 exhaustive qui essaye toutes les combinaisons de transactions et une approche plus tactique.
 Quelle est sa complexité ?
 --
-
- */
+*/
 
 const BLOC_SIZE = 6000;
 
-//transaction map "size: reward"
+// raw data saved in a Map data structure. Map is a good alternative to Object literal as type of keys
+// is not converted to string. key is a transaction size in bytes and value is the reward in satoshis
 const transactionMap = new Map([
     [2000, 13000],
     [6000, 9000],
@@ -30,4 +30,24 @@ const transactionMap = new Map([
     [600, 1500]
 ]);
 
-console.log(transactionMap.keys());
+//A transaction is defined by a size, a reward and a ratio satoshis/bits
+class Transaction {
+    constructor(size, reward){
+        this.size = size;
+        this.reward = reward;
+        this.ratio = reward / size; // Higher is the ratio, better is our transaction!
+    }
+}
+
+//Fill up a transactions list with Transaction object constructed with data from transactionMap
+const transactions = Array.from(transactionMap.entries()).map(([size, reward]) => new Transaction(size, reward));
+
+/*
+    Solution 1:
+    Try all the possible combinations and take the one with the higher reward
+    If we found more than 1 solutions, take the one with the less transactions.
+    We use recursive binary search for this.
+ */
+
+
+console.log(transactions);
