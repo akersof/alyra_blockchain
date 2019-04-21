574cb03566623e9e96985059c69ec2ae71537bdf
akersof <akersof@gmail.com>
2019-04-21 13:45:33 +0200

    1.3.2 done

--- chapter1/1-3-2.js
+++ chapter1/1-3-2.js
@@ -1,6 +1,28 @@
-const frequency = str => {
-    let result= [];
-    for(let i = 0; i <= str; ++i) {
+class Occur {
+    constructor(letter, freq = 1) {
+        this.letter = letter;
+        this.freq = freq;
+    }
+    inc() { ++this.freq;}
+}
 
-  }
+const frequency = str => {
+    let result= []; //array of Occur objects
+    //fill our result array;
+    for(let i = 0; i < str.length; ++i) {
+        let found = result.find(elem => elem.letter === str[i]);
+        found ? found.inc() : result.push(new Occur(str[i]));
+    }
+    result.sort((elem1, elem2) => elem2.freq > elem1.freq ? 1: -1);
+    console.log(result);
+    //generate and return the output
+    const prettyPrint= () => {
+        let output = "";
+        result.forEach((elem) => output += `${elem.freq} ${elem.letter}\t`);
+        return output;
+    };
+    //generate our output;
+    return prettyPrint();
 };
+
+console.log(frequency("Etre contesté, c’est être constaté"));
