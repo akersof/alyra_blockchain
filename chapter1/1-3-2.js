18202bd3a7244a12b572a67df0760842f70b1dd2
akersof <akersof@gmail.com>
2019-04-21 13:54:11 +0200

    1.3.2 done, with some fixes

--- chapter1/1-3-2.js
+++ chapter1/1-3-2.js
@@ -13,8 +13,8 @@ const frequency = str => {
         let found = result.find(elem => elem.letter === str[i]);
         found ? found.inc() : result.push(new Occur(str[i]));
     }
+    //sort our array in descend way.
     result.sort((elem1, elem2) => elem2.freq > elem1.freq ? 1: -1);
-    console.log(result);
     //generate and return the output
     const prettyPrint= () => {
         let output = "";
