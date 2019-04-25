0826a06bfb618871c6b1be24e8496d678210c299
akersof <akersof@gmail.com>
2019-04-25 10:05:15 +0200

    cleaning EllipticCurve library

--- chapter1/1-3-4.js
+++ chapter1/1-3-4.js
@@ -1,23 +1,38 @@
 const crypto = require("crypto");
+
+class Point {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
 class EllipticCurve {
+    //y^2 = x^3 + a * x + b
     constructor(a, b) {
         this.a = a;
         this.b = b;
         if( 4 * (Math.pow(a, 3)) + 27 * (Math.pow(b, 2)) === 0) throw new Error("Not a valid elliptic curve");
     }
+    // Check if 2 elliptic curve are equals
     eq(curve){
         return (this.a === curve.a && this.b === curve.b);
     }
-    testPoint(abs, ord){
-        if(Math.pow(ord, 2) === (Math.pow(abs, 3) + abs * this.a   + this.b))
-            return true;
+    // Check if point is present on the elliptic curve.
+    isPoint(point){
+        return Math.pow(point.y, 2) === (Math.pow(point.x, 3) + point.x * this.a + this.b);
+
     }
+    // return a the equation of the elliptic curve as string
     str(){
-        return `y^2 = x^3 + ${this.a}x + ${this.b}`;
+        let str = "y^2 = x^3";
+        str = str + (this.a === 0 ? "" : ` + ${this.a}x`);
+        str = str + (this.b === 0 ? "" : ` + ${this.b}`);
+        return str;
     }
-    f(x) {
-        return Math.sqrt(Math.pow(x, 3) + this.a * x + this.b);
-    }
+    //f(x) {
+    //    return Math.sqrt(Math.pow(x, 3) + this.a * x + this.b);
+    //}
 
     mul(n, Xg, Yg){
         let m = 0;
@@ -32,12 +47,11 @@ class EllipticCurve {
 }
 
 //Input
-const a = 10;
-const b = 11;
+const secp256k1 = new EllipticCurve(0, 7);
 
-const curve = new EllipticCurve(a, b);
-const Xg = parseInt(crypto.randomBytes(1).toString('hex'), 16);
-const Yg = curve.f(Xg);
-console.log('Gx=',Xg ,'Gy=', Yg);
-console.log(`G is on ellipitc curve?: ${curve.testPoint(Xg, Yg)}`);
+
+//const Xg = parseInt(crypto.randomBytes(1).toString('hex'), 16);
+//const Yg = curve.f(Xg);
+//console.log('Gx=',Xg ,'Gy=', Yg);
+//console.log(`G is on ellipitc curve?: ${curve.testPoint(Xg, Yg)}`);
 
