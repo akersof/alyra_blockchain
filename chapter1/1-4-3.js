701b4924e1cb8800c4eb80f43b6b71db3c0e0fe4
akersof <akersof@gmail.com>
2019-04-28 23:36:48 +0200

    1.4.3 done

--- chapter1/1-4-3.js
+++ chapter1/1-4-3.js
@@ -1,24 +1,13 @@
-const PREV_TX_SIZE = 32;
-const EXIT_INDEX_SIZE = 4;
-const SEQ_SIZE = 4;
-
-//little endian to big endian
-//take a string and return an Int
-// TODO works as long as this is a 4 bytes number ?
+//little endian hex string to big endian
 const le2be = str => {
-    let beString = "";
+    let beStr = "";
     for(let i = 0; i < str.length; i += 2)
-        beString = str[i] + str[i + 1] + beString;
-    return beString;
-};
-
-//take a VarInt as hexString and return the value
-const readVarInt = str =>{
-
+        beStr = str[i] + str[i + 1] + beStr;
+    return beStr;
 };
 
 //take a buffer with the VarInt field at index 0, and return a list containing,
-//the size of the VarInt field, and the value associated to this varInt
+//the size of the VarInt field itself (prefix with value), and the size in coming data
 const readVarIntField = buff => {
     let viSize = 1;
     let viValue = 0;
@@ -26,53 +15,61 @@ const readVarIntField = buff => {
     switch(prefix) {
         case 'FD':
             viSize += 2;
+            viValue = parseInt(buff.slice(1, 1 + viSize).toString('hex'), 16);
             break;
         case 'FE':
             viSize += 4;
+            viValue = parseInt(buff.slice(1, 1 + viSize).toString('hex'), 16);
             break;
         case 'FF':
             viSize += 8;
+            viValue = parseInt(buff.slice(1, 1 + viSize).toString('hex'), 16);
             break;
         default:
+            //the prefix we read is in fact the value here cause no prefix.
             viValue = parseInt(prefix, 16);
             break;
     }
     return [viSize, viValue];
 };
 
+//Input related constants
+const TXID_SIZE = 32;
+const OUTPUT_INDEX_SIZE = 4;
+const SEQ_SIZE = 4;
+
 //A starting working example of an Entry class.
 //Just enough for 1.4.3 exercice done.
-class Entry {
+class Input {
     constructor() {
-        this.prevTx = "";
-        this.index = "";
+        this.TXID = "";
+        this.outIndex = "";
         this.scriptSig = "";
         this.sequence = "";
     }
     //construct an entry from hex string.
-    // TODO: can be done better. Try to check old code on PE and ELF disassembler.
     from(entry){
         let curIndex = 0;
         let buff = Buffer.from(entry, 'hex');
-        this.prevTx = buff.slice(curIndex, curIndex + PREV_TX_SIZE).toString('hex');
-        curIndex += PREV_TX_SIZE;
-        let index = buff.slice(curIndex, curIndex + EXIT_INDEX_SIZE).toString('hex');//.toString('hex');
-        this.index = parseInt(le2be(index), 16);//.parseInt(16);//.reverse().join().parseInt(16);
-        curIndex += EXIT_INDEX_SIZE;
+        //Get previous transaction ID
+        this.TXID = le2be(buff.slice(curIndex, curIndex + TXID_SIZE).toString('hex'));
+        curIndex += TXID_SIZE;
+        //get output index
+        this.outIndex = parseInt(le2be(buff.slice(curIndex, curIndex + OUTPUT_INDEX_SIZE).toString('hex')), 16);//.toString('hex');
+        curIndex += OUTPUT_INDEX_SIZE;
+        //Get scriptSig
         let [viSize, viValue] = readVarIntField(buff.slice(curIndex));
-        console.log(viSize);
-        console.log(viValue);
         curIndex += viSize;
         this.scriptSig = buff.slice(curIndex, curIndex + viValue).toString('hex');
         curIndex += viValue;
         this.sequence = buff.slice(curIndex, curIndex + SEQ_SIZE).toString('hex');
     }
     str(){
-        let prevTx = `previous tx: ${this.prevTx}`;
-        let index = `index: ${this.index}`;
+        let TXID = `previous tx: ${this.TXID}`;
+        let outIndex = `output index: ${this.outIndex}`;
         let scriptSig = `scriptSig: ${this.scriptSig}`;
         let sequence = `sequence: ${this.sequence}`;
-        return `${prevTx}\n${index}\n${scriptSig}\n${sequence}\n`;
+        return `${TXID}\n${outIndex}\n${scriptSig}\n${sequence}\n`;
     }
 }
 
@@ -81,6 +78,6 @@ const exercice =
     "25e6d85c03cf301bc16465e3176b55bba9727706819eaf07cf84cf52d02203f7dc7ae9ab36bead14dd3c83c8c030bf8" +
     "ce596e692021b66441b39b4b35e64e012102f63ae3eba460a8ed1be568b0c9a6c947abe9f079bcf861a7fdb2fd577ed" +
     "48a81Feffffff";
-let entry = new Entry();
-entry.from(exercice);
-console.log(entry.str());
+let input = new Input();
+input.from(exercice);
+console.log(input.str());
--- chapter1/1-4-4.js
+++ chapter1/1-4-4.js
@@ -7,6 +7,71 @@ const le2be = str => {
     return beStr;
 };
 
+//take a buffer with the VarInt field at index 0, and return a list containing,
+//the size of the VarInt field itself (prefix with value), and the size in coming data
+const readVarIntField = buff => {
+    let viSize = 1;
+    let viValue = 0;
+    let prefix = buff[0].toString(16).toUpperCase();
+    switch(prefix) {
+        case 'FD':
+            viSize += 2;
+            viValue = parseInt(buff.slice(1, 1 + viSize).toString('hex'), 16);
+            break;
+        case 'FE':
+            viSize += 4;
+            viValue = parseInt(buff.slice(1, 1 + viSize).toString('hex'), 16);
+            break;
+        case 'FF':
+            viSize += 8;
+            viValue = parseInt(buff.slice(1, 1 + viSize).toString('hex'), 16);
+            break;
+        default:
+            //the prefix we read is in fact the value here cause no prefix.
+            viValue = parseInt(prefix, 16);
+            break;
+    }
+    return [viSize, viValue];
+};
+
+//Input related constants
+const TXID_SIZE = 32;
+const OUTPUT_INDEX_SIZE = 4;
+const SEQ_SIZE = 4;
+
+class Input {
+    constructor() {
+        this.TXID = "";
+        this.outIndex = "";
+        this.scriptSig = "";
+        this.sequence = "";
+    }
+    //construct an entry from hex string.
+    from(entry){
+        let curIndex = 0;
+        let buff = Buffer.from(entry, 'hex');
+        //Get previous transaction ID
+        this.TXID = le2be(buff.slice(curIndex, curIndex + TXID_SIZE).toString('hex'));
+        curIndex += TXID_SIZE;
+        //get output index
+        this.outIndex = parseInt(le2be(buff.slice(curIndex, curIndex + OUTPUT_INDEX_SIZE).toString('hex')), 16);//.toString('hex');
+        curIndex += OUTPUT_INDEX_SIZE;
+        //Get scriptSig
+        let [viSize, viValue] = readVarIntField(buff.slice(curIndex));
+        curIndex += viSize;
+        this.scriptSig = buff.slice(curIndex, curIndex + viValue).toString('hex');
+        curIndex += viValue;
+        this.sequence = buff.slice(curIndex, curIndex + SEQ_SIZE).toString('hex');
+    }
+    str(){
+        let TXID = `previous tx: ${this.TXID}`;
+        let outIndex = `output index: ${this.outIndex}`;
+        let scriptSig = `scriptSig: ${this.scriptSig}`;
+        let sequence = `sequence: ${this.sequence}`;
+        return `${TXID}\n${outIndex}\n${scriptSig}\n${sequence}\n`;
+    }
+}
+
 //Transaction related constants
 const VERSION_SIZE = 4;
 
@@ -14,7 +79,8 @@ class Transaction{
     constructor() {
         this.version = 0;
         this.inputCount = 0;
-        this.entries = []; //an array of entry
+        this.inputs = []; //an array of entry
+        this.outputs = [];
     }
     from(str){
         let curIndex = 0;
@@ -22,13 +88,19 @@ class Transaction{
         //Get the version
         this.version = parseInt(le2be(buff.slice(curIndex, VERSION_SIZE).toString('hex')), 16);
         curIndex += VERSION_SIZE;
-        //Get inputCount
+        //get InputCount
+        let [fieldSize, viValue] = readVarIntField(buff.slice(curIndex));
+        this.inputCount = viValue;
+        curIndex += fieldSize;
+        // get inputs
+
 
 
     }
     str(){
         let version = `version: ${this.version}`;
-        return `${version}\n`;
+        let inputCount = `nb input: ${this.inputCount}`;
+        return `${version}\n${inputCount}`;
     }
 }
 
@@ -40,6 +112,16 @@ const exercice = "0100000001f129de033c57582efb464e94ad438fff493cc4de4481729b8597
     "f97672d9acb5130ec4f888ac18411a000000000017a9140b8372dffcb39943c7bfca84" +
     "f9c40763b8fa9a068700000000";
 
+const inputExample =
+    "941e985075825e09de53b08cdd346bb67075ef0ce5c94f98853292d4bf94c10d010000006b483045022100ab44ef4" +
+    "25e6d85c03cf301bc16465e3176b55bba9727706819eaf07cf84cf52d02203f7dc7ae9ab36bead14dd3c83c8c030bf8" +
+    "ce596e692021b66441b39b4b35e64e012102f63ae3eba460a8ed1be568b0c9a6c947abe9f079bcf861a7fdb2fd577ed" +
+    "48a81Feffffff";
+
 let transaction = new Transaction();
 transaction.from(exercice);
 console.log(transaction.str());
+
+let input = new Input();
+input.from(inputExample);
+console.log(input.str());
