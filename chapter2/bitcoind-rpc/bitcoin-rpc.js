22d1813f02814040caa8ea2c76bff6fc6e4c7a88
akersof <akersof@gmail.com>
2019-05-03 12:29:09 +0200

    main rpc bitcoind command done

--- chapter2/bitcoind-rpc/bitcoin-rpc.js
+++ chapter2/bitcoind-rpc/bitcoin-rpc.js
@@ -5,6 +5,14 @@ const range = (start, end) => {
     return Array(end - start + 1).fill().map((_, idx) => start + idx)
 };
 
+const le2be = str => {
+    let beStr = "";
+    for(let i = 0; i < str.length; i += 2)
+        beStr = str[i] + str[i + 1] + beStr;
+    return beStr;
+};
+
+
 class BitcoinClient {
     // #TODO: read these values from a config file too;
     constructor(network = 'regtest', port = 18443, username = 'bitcoin', password = 'bitcoin') {
@@ -41,6 +49,9 @@ class BitcoinClient {
     async getBlock(hash) {
         return this.connection.getBlock(hash, true);
     }
+    async getBlocByHeight(height) {
+        return this.getBlockHash(height).then((hash) => this.getBlock(hash));
+    }
     async getLastBlock(n) {
         let lstHash = await this.getLastBlockHash(n);
         return Promise.all(lstHash.map((hash) => this.getBlock(hash)));
@@ -48,61 +59,21 @@ class BitcoinClient {
     async decodeRawTransaction(hexStr) {
         return this.connection.decodeRawTransaction(hexStr);
     }
+    async getTransaction(txid) {
+        return this.connection.getTransaction(txid);
+    }
+    async getTransactionsByBlockHash(hash) {
+        let jsonBlock = await this.getBlock(hash);
+        if(parseInt(jsonBlock['nTx']) >= 1) {
+           return Promise.all(jsonBlock['tx'].map((txid) => this.getTransaction(txid)));
+        } else return [];
+    }
+    async getTransactionsByBlockHeight(height) {
+        return this.getBlockHash(height).then((hash) => this.getTransactionsByBlockHash(hash));
+
+    }
+    async getBlockchainInfo() {
+        return this.connection.getBlockchainInfo();
+    }
 }
-const bc = new BitcoinClient();
-//bc.getLastBlock(5).then(console.log);
 
-const thai = "0200000001ec774267aa31840ffccdb3bd9eabd1" +
-    "8d7939ff3f886f240a7671be1bc9e65822000000" +
-    "00fd1f0100483045022100de9971ad9edbf07725" +
-    "f186e35bb2f494e202b42f6cc8f4f20bb00f88dd" +
-    "daef6e0220447cc3a6e3cde7140767a358c30738" +
-    "99a145fa2d618edf8aac052b4617766b79014730" +
-    "4402201964730e5ba682064cc45464e71d250357620ae2af7540c1c72e91efb5f65ee40220420dad875" +
-    "996ce076420149babbd2c0e81d6330aaec0b72f1c41805602ad065b014c8b522103745c9aceb84dcdeddf2c" +
-    "3cdc1edb0b0b5af2f9bf85612d73fa6394758eaee35d21027efbabf425077cdbceb73f6681c7ebe2ade74a65ea57e" +
-    "bcf0c42364d3822c59021023a11cfcedb993ff2e7523f92e359c4454072a66d42e8b74b4b27a8a1258abdd" +
-    "d2102e9d617f38f8c3ab9a6bde36ce991bafb295d7adba457699f8620c8160ec9e87a54aeffffffff01605a" +
-    "f405000000001600140b85d9b75f55ad9d42ea2ae9a245568ca34932f600000000";
-bc.decodeRawTransaction(thai).then(console.log);
-
-
-
-
-
-/*
-// const bitcoinCli = new Client({ network: 'regtest',port:'18443', username: 'bitcoin', password: 'bitcoin'});
-
-const getBlockCount = () => {
-    return client.getBlockCount();
-};
-
-const getBlockHashNth = async (n) => {
-    let nb = await getBlockCount();
-    if(n > nb)
-        throw new Error(`${nb} blocks in blockchain, you can't get the ${n}th block`);
-    return client.getBlockHash(n);
-};
-
-const getBlockByHash = async (hash) => {
-    return client.getBlockByHash(hash, {extension: 'json'});
-};
-
-
-
-
-
-
-const thai = "0200000001ec774267aa31840ffccdb3bd9eabd1" +
-    "8d7939ff3f886f240a7671be1bc9e65822000000" +
-    "00fd1f0100483045022100de9971ad9edbf07725" +
-    "f186e35bb2f494e202b42f6cc8f4f20bb00f88dd" +
-    "daef6e0220447cc3a6e3cde7140767a358c30738" +
-    "99a145fa2d618edf8aac052b4617766b79014730" +
-    "4402201964730e5ba682064cc45464e71d250357620ae2af7540c1c72e91efb5f65ee40220420dad875996ce076420149babbd2c0e81d6330aaec0b72f1c41805602ad065b014c8b522103745c9aceb84dcdeddf2c3cdc1edb0b0b5af2f9bf85612d73fa6394758eaee35d21027efbabf425077cdbceb73f6681c7ebe2ade74a65ea57ebcf0c42364d3822c59021023a11cfcedb993ff2e7523f92e359c4454072a66d42e8b74b4b27a8a1258abddd2102e9d617f38f8c3ab9a6bde36ce991bafb295d7adba457699f8620c8160ec9e87a54aeffffffff01605af405000000001600140b85d9b75f55ad9d42ea2ae9a245568ca34932f600000000";
-
-//client.decodeRawTransaction(thai, true).then(console.log);
-
-//getBlockCount().then(console.log);
-//getBlockHashNth(107).then((hash) => client.getBlockByHash(hash)).then(console.log);
-*/
