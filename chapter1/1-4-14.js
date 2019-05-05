6b272e95f7c9b75e963125b275554ecef117198f
akersof <akersof@gmail.com>
2019-05-05 15:43:35 +0200

    1.4.14 done

--- chapter1/1-4-14.js
+++ chapter1/1-4-14.js
@@ -7,6 +7,8 @@ b730c5cff987146330508c88cc3e642d1b9f5154854764fd547e0a54eaf26849ffff001d2e4a4c3d
 cb3ef9eda138e4587e7afd31e7f434cbb6837e17feb0c5ac00000000”)-> true
  */
 
+const crypto = require('crypto');
+
 const VERSION_SIZE = 4;
 const HASH_BLOCK_SIZE = 32;
 const MERKLE_SIZE = 32;
@@ -30,7 +32,7 @@ class BlockHeader{
         this.timestamp = "";
         this.bits = "";
         this.nonce = "";
-
+        this.hex = "";
     }
     from(hexStr) {
         if(hexStr.toUpperCase().startsWith('0X'))
@@ -49,6 +51,7 @@ class BlockHeader{
         index += BITS_SIZE;
         this.nonce = le2be(buff.slice(index, index + NONCE_SIZE).toString('hex'));
         index += NONCE_SIZE;
+        this.hex = buff.slice(0, index).toString('hex');
     }
     str() {
         let version = `version: ${this.version}`;
@@ -59,8 +62,17 @@ class BlockHeader{
         let date = `date: ${new Date(this.timestamp * 1000).toGMTString()}`;
         let bits = `bits: ${this.bits}`;
         let nonce = `nonce: ${this.nonce}`;
+        let hex = `hex: ${this.hex}`;
 
-        return `${version}\n${versionHex}\n${prevBlocHash}\n${merkleRoot}\n${timeStamp}\n${date}\n${bits}\n${nonce}`;
+        return `${version}\n${versionHex}\n${prevBlocHash}\n${merkleRoot}\n${timeStamp}\n${date}\n${bits}\n${nonce}\n${hex}`;
+    }
+    checkPOW() {
+        let blockHash = le2be(crypto.createHash('sha256').update(crypto.createHash('sha256')
+                            .update(Buffer.from(this.hex, 'hex')).digest()).digest('hex'));
+        let coef = BigInt("0x" + this.bits.slice(2));
+        let exp = BigInt("0x" + this.bits.slice(0, 2));
+        let target = coef * 2n ** (8n * (exp-3n));
+        return BigInt('0x' + blockHash) < target;
     }
 }
 
@@ -73,3 +85,4 @@ const sample = "0x01000000008de6ae7a37b4f26a763f4d65c5bc7feb1ad9e3ce0fff4190c067
 const blockHeader = new BlockHeader();
 blockHeader.from(sample);
 console.log(blockHeader.str());
+console.log('Proof of work: ', blockHeader.checkPOW());
