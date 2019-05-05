a39070727e800bec70fabf77922cfea90063897a
akersof <akersof@gmail.com>
2019-05-06 00:05:30 +0200

    first smart contract

---
+++ chapter3/intro.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.7;
+
+contract SceneOuverte {
+    string[12] public passagesArtistes;
+    uint public freeSlot = 12;
+    uint tour;
+    //memory = this string doesn't exist int the blockchain and is stored in memory only
+    function sInscrire(string memory nomDArtiste) public {
+        require (freeSlot >= 0);
+        passagesArtistes[12 - freeSlot] = nomDArtiste;
+        freeSlot--;
+    }
+    function passerArtisteSuivant() public{
+        tour++;
+    }
+    function artisteEnCours() public view returns (string memory) {
+        require(tour < 12 - freeSlot);
+        return passagesArtistes[tour];
+    }
+}
