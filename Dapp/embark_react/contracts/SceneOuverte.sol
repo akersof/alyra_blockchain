e2306d9adb962fbd806db509f0298f5ec192f906
akersof <akersof@gmail.com>
2019-05-08 10:23:11 +0200

    emmanuel courses on dapp

---
+++ Dapp/embark_react/contracts/SceneOuverte.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.7;
+
+contract SceneOuverte {
+    string[12] public passagesArtistes;
+    uint public freeSlot = 12;
+    uint tour;
+    //memory = this string doesn't exist int the blockchain and is stored in memory only
+    function sInscrire(string memory nomDArtiste) public {
+        require (freeSlot > 0);
+        passagesArtistes[12 - freeSlot] = nomDArtiste;
+        freeSlot--;
+        //alternative with if
+        //if(freeSlot > 0) {
+        //..etc
+        //}
+    }
+    function passerArtisteSuivant() public{
+        tour++;
+    }
+    function artisteEnCours() public view returns (string memory) {
+        require(tour < 12 - freeSlot);
+        return passagesArtistes[tour];
+    }
+}
