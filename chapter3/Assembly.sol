309ff23539a8254fd36dacb65a05726e69297a05
akersof <akersof@gmail.com>
2019-05-09 16:05:55 +0200

    ex 3.1.4 done

--- chapter3/Assembly.sol
+++ chapter3/Assembly.sol
@@ -1,19 +1,26 @@
 pragma solidity ^0.5.7;
 contract Assembly {
+    struct Proposal{
+        string description;
+        uint forVotes;
+        uint againstVotes;
+        uint blankVotes;
+        uint maxDate;
+        mapping (address => bool) aVote;
+    }
+    Proposal[] proposals;
+
     address[] members;
-    string[] proposals;
-    uint[] forVotes;
-    uint[] againstVotes;
-    uint[] blankVotes;
+
     // internal constant
     int constant forVote = 1;
     int constant againstVote = -1;
     int constant blankVote = 0;
+    uint constant MAX_TIMESTAMP = 604800; // 1 week
 
     function join() public {
-        if(!isMember(msg.sender)) {
-            members.push(msg.sender);
-        }
+        require(!isMember(msg.sender), "you are already a member");
+        members.push(msg.sender);
     }
 
     function isMember(address user) public view returns(bool) {
@@ -24,25 +31,30 @@ contract Assembly {
         return false;
     }
 
+    //check if a user already voted for a proposal
+    function didVote(uint indexProposal, address user) public view returns(bool) {
+        require(indexProposal < proposals.length, "out of range index");
+        return proposals[indexProposal].aVote[user];
+    }
+
     function propose(string memory proposal) public {
-        if(isMember(msg.sender)) {
-            proposals.push(proposal);
-            forVotes.push(0);
-            againstVotes.push(0);
-            blankVotes.push(0);
-        }
+        require(isMember(msg.sender), "You have to be a member to submit a proposal");
+        proposals.push(Proposal(proposal, 0, 0, 0, now + MAX_TIMESTAMP));
     }
-    // TODO: need more test on indexProposal, need to be in array range
+
     function vote(uint indexProposal, int choice) public {
-        if(isMember(msg.sender)) {
-            if(choice == forVote) forVotes[indexProposal]++;
-            else if(choice == againstVote) againstVotes[indexProposal]++;
-            else blankVotes[indexProposal]++;
-        }
+        require(indexProposal < proposals.length, "out of range index");
+        require(isMember(msg.sender), "you have to be a member to vote on a proposal");
+        require(!didVote(indexProposal, msg.sender), "you already voted for this proposal");
+        require(now <= proposals[indexProposal].maxDate, "Proposal time expired");
+        if(choice == forVote) proposals[indexProposal].forVotes++;
+        else if(choice == againstVote) proposals[indexProposal].againstVotes++;
+        else proposals[indexProposal].blankVotes++;
+        proposals[indexProposal].aVote[msg.sender] = true;
     }
-    // TODO: need more test on indexProposal, need to be in array range
     function count(uint indexProposal) public view returns(int) {
-        return int(forVotes[indexProposal]) - int(againstVotes[indexProposal]);
+        require(indexProposal < proposals.length,"out of range index");
+        return int(proposals[indexProposal].forVotes) - int(proposals[indexProposal].againstVotes);
     }
 
 }
