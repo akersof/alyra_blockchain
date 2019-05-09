03ae9ee46df1fb849528c53b8cb2d9cd3be212f3
akersof <akersof@gmail.com>
2019-05-09 23:09:50 +0200

    3.2.2 done

--- chapter3/FestivalPool.sol
+++ chapter3/FestivalPool.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.5.7;
-contract FestivalPool {
+contract Management {
     mapping (address => uint) managers;
+    uint private costs;
 
-    constructor() public {
+    constructor() internal {
         managers[msg.sender] = 100;
-
     }
 
     function transferManage(address manager, uint parts) public {
@@ -17,5 +17,38 @@ contract FestivalPool {
     function isManager(address manager) public view returns(bool) {
         if(managers[manager] > 0) return true;
         return false;
+    }
+
+    function pay(address payable dest, uint amount) public {
+        require(isManager(msg.sender));
+        require(dest != address(0));
+        require(amount > 0);
+        dest.transfer(amount);
+        costs += amount;
+    }
+
+    function () external payable {
+
+    }
+
+}
+
+contract FestivalPool is Management {
+    mapping(address => bool) mob; //mob = people going to festival
+    uint remainTickets = 100;
+    string[] public sponsors;
+
+    function sponsor(string memory name) public payable {
+        require(msg.value >= 30 ether, 'minimum 30 ethers for sponsoring');
+        sponsors.push(name);
+    }
+
+
+    function buyTicket() public payable {
+        require(!mob[msg.sender]);
+        require(msg.value >= 500 finney,"Place à 0.5 Ethers");
+        require(remainTickets > 0,"no more tickets");
+        mob[msg.sender] = true;
+        remainTickets--;
     }
 }
