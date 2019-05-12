93ba6d888d0f4c39c6efdbccc34b731fbda94d78
akersof <akersof@gmail.com>
2019-05-12 21:27:34 +0200

    3.3.2 done

--- chapter3/Pulsation.sol
+++ chapter3/Pulsation.sol
@@ -2,10 +2,25 @@ pragma solidity ^0.5.7;
 
 contract Pulsation {
     uint public beat;
-    constructor () public {
+    string private message;
+    constructor (string memory _message) public {
         beat = 0;
+        message = _message;
     }
-    function addBeat() public {
+    function addBeat() public returns(string memory) {
         beat++;
+        return message;
     }
+}
+
+contract Clock {
+    Pulsation pulse;
+    constructor(address pulseAddr) public{
+        pulse = Pulsation(pulseAddr);
+
+    }
+    function doPulse() public {
+        pulse.addBeat();
+    }
+
 }
