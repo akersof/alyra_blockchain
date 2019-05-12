c189815c44f4b9070f31751e421c60d5aeb3c23e
akersof <akersof@gmail.com>
2019-05-12 13:25:25 +0200

    3.1.5 done

--- chapter3/Assembly.sol
+++ chapter3/Assembly.sol
@@ -1,125 +1,138 @@
-// TODO: create a member struct
-// TODO: The contract creator is no more and administrator but an ower his rights can't be revoked
-// TODO: administor election follows  democracy rules
-// TODO: apparently delete array[index] create gap inside array, find a way to solve this.
-
 pragma solidity ^0.5.7;
-contract Assembly {
+
+contract Administrators {
+    mapping(address => bool) internal administrators;
+    modifier onlyAdmin() {
+        require(administrators[msg.sender] == true, 'you have to be an administrators to perform this task.');
+        _;
+    }
+    function addAdmin(address user) internal onlyAdmin {
+        administrators[user] = true;
+    }
+    //administrator self resignation
+    function resignAdmin() internal onlyAdmin {
+        administrators[msg.sender] = false;
+    }
+
+
+}
+
+contract Assembly is Administrators{
+    enum Vote {Yes, No, Blank}
+    uint constant DELAY = 604800; // 1 week
+    address public owner;
+    string public assemblyName;
+    mapping(uint => Proposal) public proposals;
+    uint proposalsCount;
+    mapping(address => Member) public members;
+
     struct Proposal{
+        uint id;
+        bool active;
         string description;
         uint forVotes;
         uint againstVotes;
         uint blankVotes;
-        uint maxDate;
+        uint delay;
         mapping (address => bool) aVote;
     }
-    Proposal[] proposals;
-    //should use a struct for members cause this is a big mess actually
-    address[] members;
-    address[] administrators;
-    string public assemblyName;
-    mapping(address => uint) warning; //counting warning on user, is >= 2 put it in blacklist
-    address[] blackList;
-
-    // internal constant
-    int constant forVote = 1;
-    int constant againstVote = -1;
-    int constant blankVote = 0;
-    uint constant MAX_TIMESTAMP = 604800; // 1 week
+    struct Member {
+        address addr;
+        bool isAdmin;
+        uint warning; //number of warning > 2 = iBlackListed true
+        bool isBlackListed;
+    }
 
     constructor(string memory name) public{
         assemblyName = name;
-        administrators.push(msg.sender);
+        owner = msg.sender;
     }
 
-    //admin functions
-    function isAdmin(address user) public view returns(bool) {
-        for(uint i = 0; i < administrators.length; i++) {
-            if(administrators[i] == user) return true;
-        }
-        return false;
+
+    modifier onlyMember() {
+        require(members[msg.sender].addr != address(0) && members[msg.sender].isBlackListed == false,
+            'you have to be a member to perform this task.');
+        _;
     }
 
-    //all administrators can promote an address for admin rights
-    function addAdmin(address user) public {
-        require(isAdmin(msg.sender), 'you are not an administrator');
-        administrators.push(user);
+    modifier onlyActiveProposal(uint indexProposal) {
+        require(proposals[indexProposal].active == true, "this proposal doesn't exists.");
+        _;
     }
 
-    //resgination
-    function resign() public {
-        require(isAdmin(msg.sender), 'you are not an administrator');
-        for(uint i = 0; i < administrators.length; ++i) {
-            if(administrators[i] == msg.sender) delete administrators[i];
-        }
+    function deleteProposal(uint indexProposal) public onlyAdmin onlyActiveProposal(indexProposal){
+        proposals[indexProposal].active = false;
     }
 
-    function deleteProposal(uint indexProposal) public {
-        require(isAdmin(msg.sender), 'you are not an administrator');
-        require(indexProposal < proposals.length, "out of range index");
-        delete proposals[indexProposal];
-    }
 
-    //warn a user, if warning >= 2 delete it and put it in blackList
-    function warningUser(address user) public {
-        require(isAdmin(msg.sender), 'you are not an administrator');
-        require(isMember(user));
-        warning[user]++;
-        if(warning[user] >= 2) {
-            //put user in blackLists
-            blackList.push(user);
-            for(uint i = 0; i < members.length; ++i) {
-                if(members[i] == user) delete members[i];
-            }
-        }
+    //warn a user, if warning >= 2 blackList the user
+    function warningUser(address user) public onlyAdmin {
+        require(members[user].addr != address(0), 'user not a member');
+        require(members[user].isBlackListed != true, 'user already black listed');
+        members[user].warning++;
+        if(members[user].warning >= 2) members[user].isBlackListed = true;
+
     }
 
     //members functions
     function join() public {
-        require(!isMember(msg.sender), "you are already a member");
-        require(!inBlackList(msg.sender), "you are in black list, you can't join anymore");
-        members.push(msg.sender);
+        require(members[msg.sender].addr == address(0), 'you are already a member');
+        members[msg.sender].addr = msg.sender;
+
     }
 
-    function inBlackList(address user) public view returns(bool) {
-        for(uint i = 0; i < blackList.length; i++) {
-            if(blackList[i] == user) return true;
-        }
-        return false;
-    }
-
-    function isMember(address user) public view returns(bool) {
-        for(uint i = 0; i < members.length; i++) {
-            if(members[i] == user) return true;
-
-        }
-        return false;
-    }
 
     //check if a user already voted for a proposal
-    function didVote(uint indexProposal, address user) public view returns(bool) {
-        require(indexProposal < proposals.length, "out of range index");
-        return proposals[indexProposal].aVote[user];
+    modifier didNotVote(uint indexProposal){
+        require(indexProposal <= proposalsCount, "out of range index");
+        require(proposals[indexProposal].aVote[msg.sender] == false, "you already voted for this proposal");
+        _;
     }
 
-    function propose(string memory proposal) public {
-        require(isMember(msg.sender), "You have to be a member to submit a proposal");
-        proposals.push(Proposal(proposal, 0, 0, 0, now + MAX_TIMESTAMP));
+    modifier onlyProposalExists(uint indexProposal) {
+        require(indexProposal <= proposalsCount, 'out of range index proposal');
+        _;
     }
 
-    function vote(uint indexProposal, int choice) public {
-        require(indexProposal < proposals.length, "out of range index");
-        require(isMember(msg.sender), "you have to be a member to vote on a proposal");
-        require(!didVote(indexProposal, msg.sender), "you already voted for this proposal");
-        require(now <= proposals[indexProposal].maxDate, "Proposal time expired");
-        if(choice == forVote) proposals[indexProposal].forVotes++;
-        else if(choice == againstVote) proposals[indexProposal].againstVotes++;
+    modifier isProposalOpen(uint indexProposal) {
+        require(now <= proposals[indexProposal].delay, 'this proposal is closed, too late.');
+        require(proposals[indexProposal].active == true, 'this proposal is not active anymore');
+        _;
+    }
+
+    function propose(string memory proposal) public onlyMember{
+        proposalsCount++;
+        proposals[proposalsCount] = Proposal(proposalsCount, true, proposal, 0, 0, 0, now + DELAY);
+    }
+
+
+    function vote(uint indexProposal, Vote vote) public
+    onlyProposalExists(indexProposal)
+    onlyMember
+    isProposalOpen(indexProposal)
+    didNotVote(indexProposal)
+    {
+        if(vote == Vote.Yes) proposals[indexProposal].forVotes++;
+        else if(vote == Vote.No) proposals[indexProposal].againstVotes++;
         else proposals[indexProposal].blankVotes++;
         proposals[indexProposal].aVote[msg.sender] = true;
     }
-    function count(uint indexProposal) public view returns(int) {
-        require(indexProposal < proposals.length,"out of range index");
+
+    function count(uint indexProposal) public onlyProposalExists(indexProposal) view returns(int){
         return int(proposals[indexProposal].forVotes) - int(proposals[indexProposal].againstVotes);
+    }
+
+    function toString(address x) internal pure returns (string memory) {
+        bytes memory b = new bytes(20);
+        for (uint i = 0; i < 20; i++)
+            b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
+        return string(b);
+    }
+
+    //Assembly administrator election
+    function election(address user) public onlyMember{
+        require(administrators[user] == false, 'user is already an administrator');
+        propose(toString(user));
     }
 
 }
