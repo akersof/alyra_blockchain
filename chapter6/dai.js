f2d10dbcba1a84b006071367aa21ae222483f186
akersof <akersof@gmail.com>
2019-05-21 12:22:42 +0200

    dai

--- chapter6/dai.js
+++ chapter6/dai.js
@@ -9,18 +9,22 @@ let dai = process.argv[4]; //number of generated DAI
 let colat = (nbEth * courEth / dai) * 100; //percentage
 let liquidCour = ((150  / 100) * dai) / nbEth; // en dollar
 
-const msg = "###################################################\n" +
+const msg1 = "###################################################\n" +
             "#################LIQUIDATOR########################\n" +
             "###################################################\n";
 
+const msg2 = "###################################################\n" +
+             "#################SPECULATOR########################\n" +
+             "###################################################\n";
+
 console.log(msg);
-console.log("colat:", colat);
+console.log("colat:", colat, "%");
 console.log("liquidCour", liquidCour, " dollars");
 
 
 let remboursement = dai / liquidCour;
 let remaining = nbEth - remboursement;
-console.log("apres remboursement de la dete:", remaining.toFixed(2), " ETH" );
+console.log("apres remboursement de la dette:", remaining.toFixed(2), " ETH" );
 let dixpourcent = (dai / 10) / liquidCour ;
 remaining -= dixpourcent;
 console.log("apres pool : ", remaining.toFixed(2), "ETH");
