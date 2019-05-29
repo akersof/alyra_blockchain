3719891e3094a7e05695b275b8615b95705e53b6
akersof <akersof@gmail.com>
2019-05-29 13:17:20 +0200

    adding custom hook for smarcontract function

--- chapter4/challenge2/src/hooks/interactContract.js
+++ chapter4/challenge2/src/hooks/interactContract.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-
 export const useIsRegistered = (dapp) =>  {
     const [isRegistered, setRegistered] = useState({});
     const [loading, setLoading] = useState(true);
